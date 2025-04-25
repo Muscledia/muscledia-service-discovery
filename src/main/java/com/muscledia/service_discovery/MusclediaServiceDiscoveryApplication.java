0a559c8082dfd73bafca0e908be8f0d7be1e67af
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-25 14:27:23 +0200

    initial commit

---
+++ src/main/java/com/muscledia/service_discovery/MusclediaServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.muscledia.service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class MusclediaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclediaServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muscledia/service_discovery/MusclediaServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscledia.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusclediaServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
